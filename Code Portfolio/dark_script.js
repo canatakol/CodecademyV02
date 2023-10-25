3f84ee0d499c596ffe0e6645b797f7a831b7e9f1
canatakol <omer.can.atakol@gmail.com>
2023-10-25 12:18:29 +0300

    Add Dark-mode for projects

--- Code Portfolio/dark_script.js
+++ Code Portfolio/dark_script.js
@@ -1,8 +1,5 @@
 const darkModeBtn = document.getElementById("darkMode");
-const body = document.getElementsByClassName("project");
-
-for (let i = 0; i < project.length; i++) {
-    const projectElement = project[i];
+const body = document.body;
 
 darkModeBtn.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
